d4562a77062a19a8707d81d79c61e1e0dce90dd5
Juliana Sidegum <jsidegum@gmail.com>
2023-05-31 15:17:28 -0300

    Ajuste rotas dependentes de autenticação.

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../views/login';
 import CadastrarUsuario from '../views/cadastrarUsuario';
 import Home from '../views/home';
+
+const isUsuarioAutenticado = false; // provisorio
 
 const Rotas = () => {
     return (
         <Router>
             <Routes>
+
+                {/* Independentes de autenticação */}
                 <Route exact path="/" element={<Login />} />
                 <Route exact path="/cadastrar-usuario" element={<CadastrarUsuario />} />
-                <Route exact path="/home" element={<Home />} />
+
+                {/* Dependentes de autenticação */}
+                {isUsuarioAutenticado ? (
+                    <Route exact path="/home" element={<Home />} />
+                ) : (
+                    <Route path="/*" element={<Navigate to="/" />} />
+                )}
+
             </Routes>
         </Router>
     );
