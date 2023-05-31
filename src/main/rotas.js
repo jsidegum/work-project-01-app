d7adf34c7b25f1c32b591be6de9174b703cf9e0e
Juliana Sidegum <jsidegum@gmail.com>
2023-05-31 10:54:28 -0300

    view home

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from '../views/login';
 import CadastrarUsuario from '../views/cadastrarUsuario';
+import Home from '../views/home';
 
 const Rotas = () => {
     return (
@@ -9,6 +10,7 @@ const Rotas = () => {
             <Routes>
                 <Route exact path="/" element={<Login />} />
                 <Route exact path="/cadastrar-usuario" element={<CadastrarUsuario />} />
+                <Route exact path="/home" element={<Home />} />
             </Routes>
         </Router>
     );
---
+++ src/views/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Home = () => {
+    return (
+        <Container>
+            <Row className="justify-content-center align-items-center" style={{ height: '100vh' }}>
+                <Col className="text-center">
+                    <h1>Bem-vindo a página inicial!!!</h1>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Home;
