79468712ae89a3acd162137fea580f2cd8da130b
Juliana Sidegum <jsidegum@gmail.com>
2023-06-01 14:39:54 -0300

    Ajuste no header, se o usuário estiver logado apresentar o botão Sair.

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Button } from 'react-bootstrap';
+
 
 const Header = () => {
+
+    const handleLogout = () => {
+        localStorage.removeItem('_usuario_logado');
+        console.log("Logout realizado com sucesso!");
+    };
+
+    const usuarioLogado = localStorage.getItem('_usuario_logado');
+
     return (
         <Navbar bg="primary" expand="lg" fixed="top" className="w-100">
             <Container>
                 <Navbar.Brand className="text-white text-center py-3" href="/">Meu App</Navbar.Brand>
+                {usuarioLogado && (
+                    <Button onClick={handleLogout} variant="danger" className="ml-auto" href="/sair">
+                        Sair
+                    </Button>
+                )}
             </Container>
         </Navbar>
     );
