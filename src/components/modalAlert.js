758e9a814e7d628df228a472a8cea4293c4597f5
Juliana Sidegum <jsidegum@gmail.com>
2023-06-01 15:02:58 -0300

    Ajuste component modalAlert

--- src/components/modalAlert.js
+++ src/components/modalAlert.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
-const ModalAlert = ({ titulo, mensagem, handleClose }) => {
+const ModalAlert = ({ mensagem, handleClose }) => {
     return (
-        <Modal show={true} onHide={handleClose}>
+        <Modal show={true} onHide={handleClose} className="alert-modal">
             <Modal.Header closeButton>
-                <Modal.Title>{titulo}</Modal.Title>
+                <Modal.Title>Alerta</Modal.Title>
             </Modal.Header>
             <Modal.Body>{mensagem}</Modal.Body>
             <Modal.Footer>
