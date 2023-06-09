390479edcf9bc3ba70ca5d46d2de7ee3b8892bed
Juliana Sidegum <jsidegum@gmail.com>
2023-06-09 16:54:31 -0300

    Inserido menu no header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Navbar, Container } from 'react-bootstrap';
-import { IoLogOutOutline } from 'react-icons/io5';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
     const handleLogout = () => {
@@ -14,18 +13,21 @@ const Header = () => {
     return (
         <Navbar bg="primary" expand="lg" fixed="top" className="w-100">
             <Container>
-                <Navbar.Brand className="text-white text-center py-3" href={usuarioLogado ? "/home" : "/"}> Meu App</Navbar.Brand>
-                {usuarioLogado && (
-                    <div style={{ color: 'white' }}>
-                        {usuarioLogado.replace(/['"]+/g, ' ')}
-                        <IoLogOutOutline
-                            size={28}
-                            onClick={handleLogout}
-                            className="ml-auto text-white"
-                            style={{ cursor: 'pointer' }}
-                        />
-                    </div>
-                )}
+                <Navbar.Brand className="text-white text-center py-3" href={usuarioLogado ? "/home" : "/"}>Meu App</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-nav" />
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="ml-auto">
+                        {usuarioLogado && (
+                            <>
+                                <Nav.Link className="text-white" href="/home">Home</Nav.Link>
+                                <Nav.Link className="text-white" href="/cadastrar-usuario">Usuários</Nav.Link>
+                                <Nav.Link className="text-white" href="/cadastrar-contatos">Contatos</Nav.Link>
+                                <Nav.Link className="text-white" onClick={handleLogout}>Sair</Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+
             </Container>
         </Navbar>
     );
